77fd111b195f7b2f0a68b03c77b2f3465dea428d
Bboddy <bryce.boddy@gmail.com>
2020-07-22 04:45:22 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :views, "app/views"
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, "un-hackable-secret"
   end
 
   get "/" do
