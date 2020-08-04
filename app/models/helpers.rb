4fad79e5288aa6cebcbc111b041de792d2945082
Kenlyn Terai <kenlyn.terai@gmail.com>
2020-08-03 19:26:12 -0700

    Remove redundant code and prefer placement of these helpers in Application Controller

--- app/models/helpers.rb
+++ app/models/helpers.rb
@@ -1,11 +1,11 @@
 require 'sinatra/base'
 
 class Helpers
-    def self.current_user(args)
-        @user ||= User.find(args[:user_id])
-    end
+#     def self.current_user(args)
+#         @user ||= User.find(args[:user_id])
+#     end
 
-    def self.is_logged_in?(args)
-        args.include?(:user_id)
-    end
+#     def self.is_logged_in?(args)
+#         args.include?(:user_id)
+#     end
 end
